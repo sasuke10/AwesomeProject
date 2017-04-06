f0ecf8eccb4fc0747b60991d0391f526697609a3
sasuke <sasuke@Mac-mini-de-LOIS.local>
2017-04-06 18:26:15 -0500

    Ya quedo el mapa en IOS, falta configuracion android

--- src/views/TestView.js
+++ src/views/TestView.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Platform,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import MapView from 'react-native-maps';
 
+const isAndroid = Platform.OS === 'android';
 
 export default class TestView extends Component {
   render() {
+    console.warn('a', MapView);
+    const stylesMap = isAndroid ? styles.mapAndroid : styles.map;
     return (
       <View style={styles.container}>
         <MapView
+          style={ stylesMap }
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
@@ -27,8 +32,21 @@ export default class TestView extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingTop: 50,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  mapAndroid: {
   },
 });
