76546916b9eb96002773384c2663e6abd786f46b
sasuke <sasuke@Mac-mini-de-LOIS.local>
2017-04-06 16:33:14 -0500

    Agregué el mapa al componente testView

--- src/views/TestView.js
+++ src/views/TestView.js
@@ -5,13 +5,21 @@ import {
   Text
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import MapView from 'react-native-maps';
 
 
 export default class TestView extends Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </View>
     );
   }
