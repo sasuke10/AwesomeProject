cb24c9963ad0a716b0e69fd694cb4cfbfa0b6217
sasuke <sasuke@Mac-mini-de-LOIS.local>
2017-04-10 17:05:05 -0500

    Incorpore el componente para Login, hay problemas de estilos

--- src/views/LoginView/SignIn.js
+++ src/views/LoginView/SignIn.js
@@ -6,8 +6,12 @@ import {
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import LinearGradient from 'react-native-linear-gradient';
+import Login from 'react-native-simple-login';
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     linearGradient: {
         flex: 1,
         alignItems: 'center',
@@ -16,6 +20,12 @@ const styles = StyleSheet.create({
         height: 100,
         width: 330,
         marginTop: 80,
+    },
+    auxContainer: {
+        flexDirection: 'row',
+        marginLeft: 40,
+        marginRight: 40,
+        flex: 1,
     },
 });
 
@@ -32,15 +42,26 @@ export default class LoginView extends Component {
         Actions.testview();
     }
 
+    onLogin(email) {
+        return () => console.warn('email', email);
+    }
 
     render() {
         return (
-            <LinearGradient
-                colors={['#f74b25', '#f32778']}
-                style={styles.linearGradient}
-                start={{ x: 1.0, y: 0.0 }} end={{ x: 0.0, y: 1.0 }} >
-                <Image style={ styles.logo } source={require('../../img/logopng.png')} />
-            </LinearGradient>
+            <View style={styles.container}>
+                <LinearGradient
+                    colors={['#f74b25', '#f32778']}
+                    style={styles.linearGradient}
+                    start={{ x: 1.0, y: 0.0 }} end={{ x: 0.0, y: 1.0 }} >
+                    <Image style={ styles.logo } source={require('../../img/logopng.png')} />
+                </LinearGradient>
+                <View style={styles.auxContainer}>
+                    <Login
+                        style={styles.loginComponent}
+                        onLogin={ this.onLogin }
+                    />
+                </View>
+            </View>
         );
     }
 }
